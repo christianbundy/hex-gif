8eb67815f36310430c5be3cce91c1e4b4173280f
Christian Bundy <christianbundy@fraction.io>
2020-01-17 08:52:52 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const fs = require('fs').promises
+
+// This is a tiny white GIF encoded with base64.
+const magicBytes = 'R0lGODlhAQABAIABAP///wAAACwAAAAAAQABAAACAkQBADs='
+const whiteGif = Buffer.from(magicBytes, 'base64')
+
+// We need to insert our color at offset 13 to replace bytes 13, 14, and 15.
+const colorOffset = 13
+const colorLength = 3
+
+// The start and end of the GIF never change, so we can repeatedly copy them.
+const gifStart = whiteGif.slice(0, colorOffset)
+const gifEnd = whiteGif.slice(colorOffset + colorLength)
+
+const hexGif = (input) => {
+  // Only convert last 6 characters, so `ffffff` and `#ffffff` are equivalent.
+  const hexColor = input.slice(-6)
+  const gifCopy = Buffer.from(whiteGif)
+  const colorBytes = Buffer.from(hexColor, 'hex')
+  return Buffer.concat([gifStart, colorBytes, gifEnd])
+}
+
+const output = hexGif('#ff00ff')
+fs.writeFile('out.gif', output).then(() => {
+  console.log('Done!')
+})
